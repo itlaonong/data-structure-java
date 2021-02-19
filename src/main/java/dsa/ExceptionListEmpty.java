a12bd5acd304653cc98cb1435531eaedeee84d9f
itlaonong <itlaonong@gmail.com>
2021-02-19 15:29:42 +0800

    chapter3

---
+++ src/main/java/dsa/ExceptionListEmpty.java
@@ -0,0 +1,7 @@
+package dsa;
+
+public class ExceptionListEmpty extends RuntimeException{
+    public ExceptionListEmpty(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/dsa/ExceptionNoSuchElement.java
@@ -0,0 +1,7 @@
+package dsa;
+
+public class ExceptionNoSuchElement extends RuntimeException {
+    public ExceptionNoSuchElement(String s) {
+        super(s);
+    }
+}
